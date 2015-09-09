fade592030ee89402e5e8b113feb878080bb0dc5
emmanuelrs <emmanuelrs@gmail.com>
2015-09-08 23:20:13 -0600

    error con la initi

--- src/cellularatomatonmain.adb
+++ src/cellularatomatonmain.adb
@@ -17,9 +17,28 @@ procedure Cellularatomatonmain is
 
    NumberToCheck : Integer := 0; ---> Users input
 
-   BinaryNumber: Inteter := 0;   ---> Binary Number of the users input
+   BinaryNumber: Integer := 0;   ---> Binary Number of the users input
 
    -------------------------------------------------------------------------------
+   Type MyArray is array(1..3) of Integer;
+
+   procedure initi (Cero:out MyArray) is
+
+     Type BinaryArray is array(1..Time) of Integer;
+
+     Cero: MyArray := (0,0,0);
+     One : MyArray := (0,0,1);
+     Two : MyArray := (0,1,0);
+     Three : MyArray := (0,1,1);
+     Four: MyArray := (1,0,0);
+     Five: MyArray := (1,0,1);
+     Six : MyArray := (1,1,0);
+     Seven : MyArray := (1,1,1);
+   begin
+      null;
+   end initi;
+
+
 
 begin
    Put("Write a number to check! : ");
@@ -28,4 +47,6 @@ begin
    Get(Time);
    Put(Time);
    New_Line;
+   initi;
+   --Put(Cero(1));
 end Cellularatomatonmain;
